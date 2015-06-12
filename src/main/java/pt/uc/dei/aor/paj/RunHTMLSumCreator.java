56b2e0e2e02dd3a0621a0772e77abfccdeda4e80
renatadiasilva <renatadiasilva@gmail.com>
2015-06-13 00:13:05 +0100

    ficheiros iniciais necessários

---
+++ src/main/java/pt/uc/dei/aor/paj/RunHTMLSumCreator.java
@@ -0,0 +1,13 @@
+package pt.uc.dei.aor.paj;
+
+public class RunHTMLSumCreator {
+
+	public static void main(String[] args) throws Exception {
+
+		//recebe string com XML transforma back to XML
+		// XSL já preparado... ok abrir em firefox
+		Subscriber r = new Subscriber();
+		r.subscribe();
+	}
+
+}
